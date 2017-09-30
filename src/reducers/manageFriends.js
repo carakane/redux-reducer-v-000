1ae64a48f43a4a8864b55c2a38e4ed640e7eb6ef
Cara Kane <cara.kane@gmail.com>
2017-09-30 15:18:38 +0000

    automatically backed up by learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,7 +1,7 @@
 export function manageFriends(state, action){
   switch(action.type){
     case 'ADD_FRIEND':
-      return {friends: [...this.state.friends, friend] };
+      return {friends: [...this.state.friends, action.friend] };
     case 'REMOVE_FRIEND':
       return {friends: this.state.friends.shift[action.id]}
     default:
