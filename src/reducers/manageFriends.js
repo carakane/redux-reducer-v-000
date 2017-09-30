dfc734fde74068ab4f9f57b18b84dd70f927e79c
Cara Kane <cara.kane@gmail.com>
2017-09-30 15:53:51 +0000

    automatically backed up by learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -5,7 +5,7 @@ export function manageFriends(state, action){
       break;
     case 'REMOVE_FRIEND':
       var friendID = state.friends.findIndex(element => element.id === action.id)
-      return {friends: state.friends.slice(, )}
+      return {friends: state.friends.slice(friendID, friendID)}
       break;
     default:
       return state;
